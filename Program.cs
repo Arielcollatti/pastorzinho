acc65f492834ae765853d053ccfcec355723571f
arielcollatti <colatti2017>
2023-03-07 13:44:13 -0300

    exercicio finalizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿
 Console.WriteLine("pastorzinho");
+
+Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+
+Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
+
+Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+
+Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
+
+    
