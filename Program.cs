c39d3111f0e92703e0b1133874b21d9c5cb38c5b
ariel colatti <126806493+Arielcollatti@users.noreply.github.com>
2023-03-18 13:34:03 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,92 @@
-﻿
-Console.WriteLine("pastorzinho");
+﻿//"Dó, ré, mi, fá, fá, fá,
+Console.WriteLine("Pastorzinho");
+Console.WriteLine();
 
-Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("Dó, "); 
+Console.Beep(1320, 200);
 
-Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
+Console.WriteLine("ré, "); 
+Console.Beep(1485, 200);
 
-Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("mi, "); 
+Console.Beep(1650, 200);
 
-Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
+Console.WriteLine("fá, "); 
+Console.Beep(1759, 200);
 
-    
+Thread.Sleep(500);
+Console.WriteLine("fá, "); 
+Console.Beep(1759, 200);
+
+Console.WriteLine("fá, "); 
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+
+
+
+Console.WriteLine();
+
+//Dó, ré, dó, ré, ré, ré,
+
+Console.WriteLine("Dó, ");
+ Console.Beep(1320, 200);
+
+Console.WriteLine("ré, "); 
+Console.Beep(1485, 200);
+
+Console.WriteLine("dó, "); 
+Console.Beep(1320, 200);
+
+Console.WriteLine("ré, ");
+ Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("ré, ");
+ Console.Beep(1485, 200);
+ 
+Console.WriteLine("ré, "); 
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+//Dó, sol, fá, mi, mi, mi,
+Console.WriteLine("Dó, "); 
+Console.Beep(1320, 200);
+
+Console.WriteLine("sol, ");
+ Console.Beep(1980, 200);
+
+Console.WriteLine("fá, "); 
+Console.Beep(1759, 200);
+
+Console.WriteLine("mi, ");
+ Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("mi, ");
+Console.Beep(1650, 200);
+
+Console.WriteLine("mi, ");
+Console.Beep(1650, 200);
+Thread.Sleep(1000);  
+
+//Dó, ré, mi, fá, fá, fá.
+Console.WriteLine("Dó,"); 
+Console.Beep(1320, 200);
+
+Console.WriteLine("ré, "); 
+Console.Beep(1485, 200);
+
+Console.WriteLine("mi,"); 
+Console.Beep(1650, 200);
+
+Console.WriteLine("fá"); 
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine("fá, ");
+Console.Beep(1759, 200);
+
+Console.WriteLine("fá. ");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
